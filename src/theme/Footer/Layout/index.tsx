e71f86b59050fdb625bac1c7bd0b3f57fb7a2902
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-20 23:12:36 +0100

    feat: add ShareButtons to footer

---
+++ src/theme/Footer/Layout/index.tsx
@@ -0,0 +1,32 @@
+import React, {type ReactNode} from 'react';
+import clsx from 'clsx';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import type {Props} from '@theme/Footer/Layout';
+import ShareButtons from '@site/src/components/ShareButtons';
+
+export default function FooterLayout({
+  style,
+  links,
+  logo,
+  copyright,
+}: Props): ReactNode {
+  return (
+    <footer
+      className={clsx(ThemeClassNames.layout.footer.container, 'footer', {
+        'footer--dark': style === 'dark',
+      })}>
+      <div className="container container-fluid">
+        {links}
+        <div style={{display: 'flex', justifyContent: 'center', marginTop: '1rem', marginBottom: '1rem'}}>
+          <ShareButtons compact={true} />
+        </div>
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && <div className="margin-bottom--sm">{logo}</div>}
+            {copyright}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
